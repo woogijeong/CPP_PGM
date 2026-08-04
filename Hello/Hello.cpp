20fa2f69a532f3afa22a867f7c77f8ffea8ded3b
Woogi <sozx2002@gamil.com>
2026-08-04 09:58:33 +0900

    test2

--- Hello/Hello.cpp
+++ Hello/Hello.cpp
@@ -1,6 +1,17 @@
 ﻿#include <iostream>
 using namespace std;
 
+int adder(int x, int y) {
+	return x + y;
+}
+
+
 int main() {
-	cout << "Hello World!" << endl;
+	int a, b;
+	cout << "첫번째 수 입력 : ";
+	cin >> a;
+	cout << "두번째 수 입력 : ";
+	cin >> b;
+
+	cout << adder(a, b);
 }
